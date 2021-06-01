4174cae6c4e32b4498f6c0581e546652de674492
dobedh <dobedh@gmail.com>
2021-06-01 23:31:23 +0900

    fireabse setting

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { firebaseInstance, authService, db } from "../fbase";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>안녕 :)</h2>
+    </div>
+  );
+}
+
+export default App;

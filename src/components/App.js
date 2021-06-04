2f5594e2f4d802806870c00ba384866ab5cf5239
dobedh <dobedh@gmail.com>
2021-06-04 23:54:57 +0900

    create user with email & password

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { firebaseInstance, authService, db } from "../fbase";
 
 function App() {
+  const [userEmail, setEmail] = useState("");
+  const [userPassword, setPassword] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const {
+      target: { email, password },
+    } = event;
+    setEmail(email.value);
+    setPassword(password.value);
+    await authService.createUserWithEmailAndPassword(userEmail, userPassword);
+  };
+
+  const onChange = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <div className="App">
-      <h3>상다미 :) </h3>
-      <form>
-        <input type="eamil" placeholder="Eamil" />
-        <input type="password" placeholder="Password" />
+      <h3> 상다미 :) </h3>
+      <form onSubmit={onSubmit}>
+        <input
+          type="eamil"
+          name="email"
+          placeholder="Eamil"
+          onChange={onChange}
+        />
+        <input type="password" name="password" placeholder="Password" />
         <input type="submit" value="로그인" />
       </form>
       <button>페이스북 로그인</button>

4a77219a5286f5926b442633649164b508057d8c
dobedh <dobedh@gmail.com>
2021-06-22 23:10:10 +0900

    posts list show

--- src/components/Community.js
+++ src/components/Community.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { db } from "../fbase";
 import { useHistory } from "react-router";
 
 const Community = () => {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    db.collection("posts").onSnapshot((snapshot) => {
+      const postArray = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setPosts(postArray);
+    });
+  }, []);
   const history = useHistory();
   const onWriteClick = () => {
     history.push("/post");
@@ -9,6 +20,11 @@ const Community = () => {
   return (
     <>
       <div>Community!</div>
+      {posts.map((post) => (
+        <div key={post.id}>
+          <h4>{post.text}</h4>
+        </div>
+      ))}
       <button onClick={onWriteClick}>글쓰기</button>
     </>
   );
